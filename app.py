8623b9966825d42156763b09c14d21a65de78d6b
nzo66 <nzo66.business@gmail.com>
2025-06-26 17:06:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -107,8 +107,8 @@ setup_proxies()
 
 # --- Configurazione Cache ---
 M3U8_CACHE = TTLCache(maxsize=200, ttl=5)
-TS_CACHE = LRUCache(maxsize=1000)
-KEY_CACHE = LRUCache(maxsize=200)
+TS_CACHE = TTLCache(maxsize=1000, ttl=600) 
+KEY_CACHE = TTLCache(maxsize=200, ttl=600)
 
 # --- Dynamic DaddyLive URL Fetcher ---
 DADDYLIVE_BASE_URL = None
