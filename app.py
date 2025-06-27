918d8461093fe0b4e4c58fee994c9e5950dc600c
nzo66 <nzo66.business@gmail.com>
2025-06-27 11:41:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from collections import defaultdict
 app = Flask(__name__)
 
 # Configurazione
-VERIFY_SSL = True
+VERIFY_SSL = False
 USER_AGENTS = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
